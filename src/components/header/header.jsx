bfeddf9321aaceb5fd4fa1c69b14ad73aa6962aa
Lucas Araujo <lucasribara@gmail.com>
2024-05-13 19:15:06 -0300

    Add user and login info foe header and add background to page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,54 @@
-import "./header.css"
+import { useSelector } from "react-redux";
 import logo from "../../assets/varanda-logov3.png"
+import { Button } from "@mui/material";
+import { Person, ShoppingBag } from '@mui/icons-material';
+import { setLogout } from "../../state";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import "./header.css"
 
-const Header = () => {
+const Header = (props) => {
+  const isAuth = Boolean(useSelector((state) => state.token));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userSection = () => {
     return (
-        <div className="header">
-            <img className="logo" alt="varanda-logo" src={logo} />
+      <div className="loggedin-user">
+        <div>
+          <Button onClick={() => dispatch(setLogout())}> Sair </Button>
         </div>
+        <div>
+          <Person fontSize="large" onClick={() => { console.log("Person") }} sx={{ "&:hover": { cursor: "pointer" }, pr: "1.5rem" }} />
+          <ShoppingBag fontSize="large" onClick={() => { console.log("Bag") }} sx={{ "&:hover": { cursor: "pointer" } }} />
+        </div>
+      </div>
     );
+  }
+  const loginButton = () => {
+    return (
+      <div className="loggedout">
+        <Button onClick={() => navigate("/login")} size="medium">
+          Login
+        </Button>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="header">
+        <img className="logo" alt="varanda-logo" src={logo} onClick={() => navigate("/")}/>
+      </div>
+      {!props.inLogin &&
+        <div className="user-bar">
+          {isAuth && userSection()}
+          {!isAuth && loginButton()}
+        </div>
+      }
+
+    </>
+  );
 }
 
 export default Header;
